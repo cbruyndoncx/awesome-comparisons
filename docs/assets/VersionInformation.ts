2e53eafb7b8b8cd01f46884e1cdbf40f668d3468
Carine Bruyndoncx <bruyndoncx@gmail.com>
2025-10-28 14:43:24 +0100

    removing deployed site first

--- docs/assets/VersionInformation.ts
+++
@@ -1,5 +0,0 @@
-export class VersionInformation {
-    public tag = 'v3.0.0-001';
-    public tagDate = '2025-10-27';
-    public tagLink = 'https://github.com/cbruyndoncx/feature-comparison/tree/v3.0.0-001';
-}
--- docs/configuration/description.md
+++
@@ -1,3 +0,0 @@
-This is a comparison of AI Coding Tools
-- Work in progress, source in Feature Grouping branch
-- Includes Code/Editor category from [AI Native Dev](http://ainativedev.io).
