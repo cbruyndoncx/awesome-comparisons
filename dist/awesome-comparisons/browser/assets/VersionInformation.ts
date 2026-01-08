45800430d0a19e9d1efbdc4fc4bac5cd9c37616a
Carine Bruyndoncx <bruyndoncx@gmail.com>
2026-01-08 15:15:18 +0100

    include dist/ in repo for ease of publishing static site

---
+++ dist/awesome-comparisons/browser/assets/VersionInformation.ts
@@ -0,0 +1,6 @@
+export class VersionInformation {
+    public version = '3.0.0-260105';
+    public tag = 'v3.0.0.260105';
+    public tagDate = '2026-01-05';
+    public tagLink = 'https://github.com/cbruyndoncx/awesome-comparisons/releases/tag/v3.0.0.260105';
+}
